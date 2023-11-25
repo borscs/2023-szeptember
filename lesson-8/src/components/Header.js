a8b67fe8011863508a7fe88c7c7e65097a0fb8ba
Borscs András <borscsa1@gmail.com>
2023-11-25 11:11:52 +0100

    lesson-8

---
+++ lesson-8/src/components/Header.js
@@ -0,0 +1,27 @@
+import classes from './Header.module.css';
+import {login, logout} from "../store/auth";
+import {useDispatch} from "react-redux";
+
+const Header = () => {
+  const dispatch = useDispatch();
+  return (
+    <header className={classes.header}>
+      <h1>Redux Auth</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href='/'>My Products</a>
+          </li>
+          <li>
+            <a href='/'>My Sales</a>
+          </li>
+          <li>
+            <button onClick={() => dispatch(logout())}>Logout</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
